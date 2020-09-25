4a1c3bed03f55266fbdb0ab55244060890a4a894
Danisa Ortanez <dortanez@yahoo.com>
2020-09-24 22:04:45 -0700

    movie rating

--- movie.js
+++ movie.js
@@ -6,7 +6,9 @@ for(let i = 1; i <= 10; i++) {
 
 $('input[type="submit"]').on('click', function(e) {
     e.preventDefault();
-    $('table').append("<tr><td>" + $('#title').val() + "</td><td>" + $('#rating').val() + "</td></tr>")
-    
-    
+    $('table').append("<tr><td>" + $('#title').val() + "</td><td>" + $('#rating').val() + "</td><td><i class='far fa-trash-alt'></i></td></tr>")
+})
+
+$('section').on('click','.far', function(e) {
+    $(this).parent().parent().remove();
 })
