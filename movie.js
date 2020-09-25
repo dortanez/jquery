cd7113bdec43bebc46a61c73aa6a32350b725b8d
Danisa Ortanez <dortanez@yahoo.com>
2020-09-25 14:32:20 -0700

    movie app

--- movie.js
+++ movie.js
@@ -1,4 +1,5 @@
 const movieArray = [];
+let currentID = 0;
 
 for(let i = 1; i <= 10; i++) {
     $('select').append('<option class="rate">' + i + '</option>')
@@ -6,9 +7,27 @@ for(let i = 1; i <= 10; i++) {
 
 $('input[type="submit"]').on('click', function(e) {
     e.preventDefault();
-    $('table').append("<tr><td>" + $('#title').val() + "</td><td>" + $('#rating').val() + "</td><td><i class='far fa-trash-alt'></i></td></tr>")
+    const title = $('#title').val();
+    const rating = $('#rating').val();
+    const movieData = {title, rating, currentID};
+    movieArray.push(movieData);
+    currentID++;
+    $('table').append(createHTMLData(movieData));
+    //trigger reset on form
 })
 
 $('section').on('click','.far', function(e) {
+    const indexId = movieArray.findIndex((val) => {val.currentID === e.target.id});
+    movieArray.splice(indexId,1);
     $(this).parent().parent().remove();
 })
+
+const createHTMLData = (data) => {
+    return `
+    <tr>
+        <td> ${data.title} </td>
+        <td> ${data.rating} </td>
+        <td><i class='far fa-trash-alt' id='${data.currentID}'></i></td>
+    </tr>
+    `
+}
