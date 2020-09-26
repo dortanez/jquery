541191843be4b09bf9afca21bd3f2af5a7de0e83
Danisa Ortanez <dortanez@yahoo.com>
2020-09-26 10:34:09 -0700

    movie app

--- movie.js
+++ movie.js
@@ -9,13 +9,13 @@ for(let i = 1; i <= 10; i++) {
 // submitting movie rating
 $('form').on('submit', function(e) {
     e.preventDefault();
-    const title = $('#title').val();
-    const rating = $('#rating').val();
+    const title = $('#title-input').val();
+    const rating = $('#rating-input').val();
     const movieData = {title, rating, currentID};
     movieArray.push(movieData);
     currentID++;
-    $('table').append(createHTMLData(movieData));
-    $(this).trigger('reset')
+    $('tbody').append(createHTMLData(movieData));
+    $(this).trigger('reset');
 })
 
 // delete rating when clicking on delete button
@@ -25,21 +25,35 @@ $('section').on('click','.far', function(e) {
     $(this).parent().parent().remove();
 })
 
-const hi = (arr) => {
-    return arr.map((val) => {
-        return 'hi'
-    })
-}
-
 // sorting movie ratings
-$('.fas').on('click', function() {
-
-
-
-
+$('.fas').on('click', function(e) {
+    const direction = $(this).hasClass('fa-sort-down') ? 'down' : 'up';
+    const keyToSortBy = $(this).attr('id');
+    const sortedMovies = sortBy(movieArray, keyToSortBy, direction);
+    $('tbody').empty();
+    for(let movie of movieArray) {
+        const HTMLToAppend = createHTMLData(movie);
+        $('tbody').append(HTMLToAppend);
+    }
     $(this).toggleClass('fa-sort-down');
     $(this).toggleClass('fa-sort-up');
 })
+
+// function that sorts the data
+const sortBy = (arr, keyToSortBy, direction) => {
+    return arr.sort((a,b) => {
+        if(keyToSortBy === 'rating') {
+            a[keyToSortBy] = +a[keyToSortBy];
+            b[keyToSortBy] = +b[keyToSortBy];
+        }
+        if(a[keyToSortBy] > b[keyToSortBy]) {
+            return direction === 'up' ? 1 : -1;
+        } else if(b[keyToSortBy] > a[keyToSortBy]) {
+            return direction === 'up' ? -1 : 1;
+        }
+        return 0;
+    })
+}
 
 // create movie rating data to append to html page
 const createHTMLData = (data) => {
